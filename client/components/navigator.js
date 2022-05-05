eaeb87ac73a1357330456d2251cd41d14dca2cb4
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-05-05 12:50:42 -0300

    config router

---
+++ client/components/navigator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default Navigator = () => {
+
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Quienes Somos</li>
+            <li>Favoritos</li>
+            <li>Contacto</li>
+        </ul>
+    )
+}
